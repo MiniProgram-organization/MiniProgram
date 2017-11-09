ec3cfbb408c328aff699acc0bb40edd0f3000103
xqgtiti <xqgtiti@163.com>
2017-11-09 13:57:55 +0800

    修改饼图

--- utils/wxcharts.js
+++ utils/wxcharts.js
@@ -18,7 +18,7 @@ var config = {
     yAxisTitleWidth: 15,
     padding: 12,
     columePadding: 3,
-    fontSize: 13,
+    fontSize: 12,
     dataPointShape: ['diamond', 'circle', 'triangle', 'rect'],
     colors: ['#7cb5ec', '#f7a35c', '#434348', '#90ed7d', '#f15c80', '#8085e9'],
     pieChartLinePadding: 25,
